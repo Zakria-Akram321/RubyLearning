849475680189b47d904aad34028e72fe1d73ef89
Zakria-Akram321 <zakria.akram999@gmail.com>
2024-03-13 16:42:51 +0500

    Loops added

---
+++ Loops-Practice/for_loop.rb
@@ -0,0 +1,9 @@
+arr = [1,2,3,4,5]
+
+for i in arr 
+  puts i
+end
+
+for i in 1..arr.length() do
+  puts arr[i]
+end
---
+++ Loops-Practice/while_loop.rb
@@ -0,0 +1,11 @@
+x = 4
+
+while x >= 0
+  y = 4 - x
+  while y >= 1
+    print "*"
+    y -= 1
+  end
+  puts
+  x -= 1
+end
