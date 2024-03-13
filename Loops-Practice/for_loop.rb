56a38fd0567e1b58766a5264c85662ac34ef81c4
Zakria-Akram321 <zakria.akram999@gmail.com>
2024-03-13 16:58:00 +0500

    All types of for loops are covered

--- Loops-Practice/for_loop.rb
+++ Loops-Practice/for_loop.rb
@@ -1,9 +1,26 @@
 arr = [1,2,3,4,5]
 
+#Simple for loop
 for i in arr 
   puts i
 end
 
+#Incrementing for loop
 for i in 1..arr.length() do
   puts arr[i]
+end
+
+#Decrementing for loop
+for i in 5.downto(1)
+  puts i
+end
+
+#For loop with incrementing step size
+for i in 1.step(5,2)
+  puts(i)
+end
+
+#For loop with decrementing step size
+for i in 5.step(1,-2)
+  puts(i)
 end
