e0b34191f31ce61dbced0a5a4415818d3dd2e60f
Zakria-Akram321 <zakria.akram999@gmail.com>
2024-03-14 13:17:17 +0500

    five most frequent words question solved

---
+++ Hashes/hash_methods.rb
@@ -0,0 +1,22 @@
+hash = {
+  dog: "canine", 
+  cat: "feline", 
+  bear: "ursine"
+}
+
+puts hash.length # prints the length of hash
+puts hash[:dog] #prints the value of dog(key)
+
+data = {
+  mcu: [
+      {name: "Iron Man", year: 2010, actors: ["Robert Downey Jr.", "Gwyneth Paltrow"]}
+  ],
+  starwars: [
+      {name: "A New Hope", year: 1977, actors: ["Mark Hamill", "Carrie Fisher"]}
+  ]
+}
+
+#To print the Gwyneth Paltrow from data
+puts data[:mcu][0][:actors][1]
+#Print Gwyneth Paltrow using dig
+puts data.dig(:mcu, 0, :actors, 1)
